6db84ff7fdc3a2da81fe0b425058573e3cc7764f
Shyank Akshar <shyankdev@gmail.com>
2023-04-15 01:44:27 +0530

    first commit

---
+++ testExmapleFramework/testExmapleFramework.h
@@ -0,0 +1,18 @@
+//
+//  testExmapleFramework.h
+//  testExmapleFramework
+//
+//  Created by Shyank Akshar on 15/04/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testExmapleFramework.
+FOUNDATION_EXPORT double testExmapleFrameworkVersionNumber;
+
+//! Project version string for testExmapleFramework.
+FOUNDATION_EXPORT const unsigned char testExmapleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testExmapleFramework/PublicHeader.h>
+
+
